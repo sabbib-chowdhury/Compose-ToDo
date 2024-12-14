941adaf74dd6a661730bbfc79b5b57b4149f9a1d
sabbib.chowdhury <sabbib.chowdhury@medibank.com.au>
2024-12-14 18:41:58 +1100

    integrate all list data to widget

--- app/src/main/java/com/wisnu/kurniawan/composetodolist/features/widgets/TodoListWidget.kt
+++ app/src/main/java/com/wisnu/kurniawan/composetodolist/features/widgets/TodoListWidget.kt
@@ -2,6 +2,7 @@ package com.wisnu.kurniawan.composetodolist.features.widgets
 
 import android.content.Context
 import android.util.Log
+import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.DpSize
@@ -10,6 +11,7 @@ import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.color.DayNightColorProvider
@@ -20,13 +22,19 @@ import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
+import com.wisnu.kurniawan.composetodolist.R
+import com.wisnu.kurniawan.composetodolist.features.todo.all.data.AllEnvironment
+import com.wisnu.kurniawan.composetodolist.foundation.extension.toColor
 import com.wisnu.kurniawan.composetodolist.foundation.theme.LightColorPalette
 import com.wisnu.kurniawan.composetodolist.foundation.theme.NightColorPalette
 import com.wisnu.kurniawan.composetodolist.foundation.theme.Shapes
 import com.wisnu.kurniawan.composetodolist.foundation.theme.Typography
+import com.wisnu.kurniawan.composetodolist.model.ToDoList
+import com.wisnu.kurniawan.composetodolist.model.ToDoStatus
 
 class TodoListWidget : GlanceAppWidget() {
 
+    private lateinit var allEnvironment: AllEnvironment
     override val sizeMode: SizeMode
         get() = SizeMode.Responsive(
             setOf(
@@ -34,43 +42,77 @@ class TodoListWidget : GlanceAppWidget() {
             )
         )
 
+    private val widgetTextStyle = TextStyle(
+        color = DayNightColorProvider(
+            LightColorPalette.onSurface,
+            NightColorPalette.onSurface
+        )
+    )
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        Log.d("LOG_TAG---", "TodoListWidget-provideGlance#40: $id")
-        provideContent {
-            MaterialTheme(
-                colorScheme =
-                if (context.isNightMode) (NightColorPalette) else LightColorPalette,
-                typography = Typography,
-                shapes = Shapes,
-            ) {
-                TodoWidget()
+        Log.d("LOG_TAG---", "TodoListWidget-provideGlance#46: ")
+        val todoList = allEnvironment.getList()
+        todoList.collect {
+            provideContent {
+                MaterialTheme(
+                    colorScheme = if (context.isNightMode) NightColorPalette else LightColorPalette,
+                    typography = Typography,
+                    shapes = Shapes,
+                ) {
+                    TodoWidget(context.getString(R.string.todo_all), it)
+                }
             }
         }
     }
 
     @Composable
-    fun TodoWidget() {
-        val widgetTextStyle = TextStyle(
-            color = DayNightColorProvider(
-                LightColorPalette.onSurface,
-                NightColorPalette.onSurface
-            )
-        )
+    fun TodoWidget(
+        toDoListName: String,
+        toDoLists: List<ToDoList> = emptyList()
+    ) {
         Column(
             modifier = GlanceModifier.fillMaxSize()
                 .padding(8.dp)
                 .background(MaterialTheme.colorScheme.surface),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-
             Text(
-                text = "Todo Widget",
+                text = toDoListName,
                 style = widgetTextStyle,
                 modifier = GlanceModifier.padding(bottom = 8.dp),
             )
-            for (i in 1..5) {
-                Text(text = "Item $i", style = widgetTextStyle)
+            TodoList(toDoLists)
+        }
+    }
+
+    @Composable
+    private fun TodoList(
+        toDoLists: List<ToDoList>,
+    ) {
+        toDoLists.forEach { todo ->
+            Text(
+                modifier = GlanceModifier.background(todo.color.toColor()),
+                text = todo.name,
+                style = widgetTextStyle
+            )
+            LazyColumn(
+                modifier = GlanceModifier.fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                items(todo.tasks) { item ->
+                    if (item.status == ToDoStatus.IN_PROGRESS)
+                        TodoItem(item.name)
+                }
             }
         }
+    }
+
+    @Composable
+    private fun TodoItem(name: String) {
+        Text(text = name, style = widgetTextStyle)
+    }
+
+    internal fun setAllEnvironment(allEnvironment: AllEnvironment) {
+        this.allEnvironment = allEnvironment
     }
 }
